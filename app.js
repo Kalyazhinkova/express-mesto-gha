53400c4bd2ee12d30e02b0beea7981fcc9b359bc
Olga Kalyazhinkova <oi.kalyazhinkova@yandex.ru>
2022-11-30 13:37:08 +0300

    Проект 14-После 2 ревью.

--- app.js
+++ app.js
@@ -13,49 +13,53 @@ import { auth } from './middlewares/auth.js';
 import { NotFoundError } from './errors/NotFoundError.js';
 
 export const run = async (envName) => {
-  process.on('unhandleRejection', (err) => {
-    console.error(err);
-    process.exit(1); // выход с ошибкой
-  });
+  try {
+    process.on('unhandleRejection', (err) => {
+      console.error(err);
+      process.exit(1); // выход с ошибкой
+    });
 
-  const config = dotenv.config({ path: path.resolve('.env.common') }).parsed;
-  if (!config) {
-    throw new Error('Config not found');
+    const config = dotenv.config({ path: path.resolve('.env.common') }).parsed;
+    if (!config) {
+      throw new Error('Config not found');
+    }
+
+    config.NODE_ENV = envName;
+
+    const app = express();
+
+    app.set('config', config);
+    app.use(bodyParser.json());
+
+    app.use('/', authRouter);
+    app.use('/users', auth, userRouter);
+    app.use('/cards', auth, cardRouter);
+    app.all('/*', () => { throw new NotFoundError('Запрашиваемая страница не найдена'); });
+    app.use(errors());
+
+    app.use((err, req, res, next) => {
+      const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+      const message = err.message || 'Неизвестная ошибка';
+      res.status(status).send({ message });
+      next();
+    });
+
+    mongoose.set('runValidators', true);
+    await mongoose.connect(config.DB_URL);
+    const server = app.listen(config.PORT, config.HOST, () => {
+      console.log(`Сервер запущен http://${config.HOST}:${config.PORT}`);
+    });
+
+    // завершаем работу приложения
+    const stop = async () => {
+      await mongoose.connection.close();
+      server.close();
+      process.exit(0); // выход без ошибок
+    };
+
+    process.on('SIGTERM', stop);
+    process.on('SIGINT', stop);
+  } catch (err) {
+    console.error(`Сервер не запущен:${err.message}`);
   }
-
-  config.NODE_ENV = envName;
-
-  const app = express();
-
-  app.set('config', config);
-  app.use(bodyParser.json());
-
-  app.use('/', authRouter);
-  app.use('/users', auth, userRouter);
-  app.use('/cards', auth, cardRouter);
-  app.all('/*', () => { throw new NotFoundError('Запрашиваемая страница не найдена'); });
-  app.use(errors());
-
-  app.use((err, req, res, next) => {
-    const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
-    const message = err.message || 'Неизвестная ошибка';
-    res.status(status).send({ message });
-    next();
-  });
-
-  mongoose.set('runValidators', true);
-  await mongoose.connect(config.DB_URL);
-  const server = app.listen(config.PORT, config.HOST, () => {
-    console.log(`Сервер запущен http://${config.HOST}:${config.PORT}`);
-  });
-
-  // завершаем работу приложения
-  const stop = async () => {
-    await mongoose.connection.close();
-    server.close();
-    process.exit(0); // выход без ошибок
-  };
-
-  process.on('SIGTERM', stop);
-  process.on('SIGINT', stop);
 };
